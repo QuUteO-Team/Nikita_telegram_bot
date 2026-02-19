6a59f58cd31087b1da24a6abecd155fd38723f0f
Nikita Sukhov <n_csg_suxov@mail.ru>
2026-02-19 16:45:40 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,28 @@
+from hammett.core import Bot, Button, Screen
+from hammett.core.constants import DEFAULT_STATE, SourceTypes
+from hammett.core.mixins import StartMixin
+
+
+class Main(StartMixin, Screen):
+    description = 'Hello, World!'
+
+    async def add_default_keyboard(self, update, context):
+        return [[
+            Button('🎸 Hello, World!', 'https://github.com/cusdeb-com/hammett',
+                   source_type=SourceTypes.URL_SOURCE_TYPE),
+        ]]
+
+def main():
+    name = 'HelloWorld'
+    app = Bot(
+        name,
+        entry_point=Main,
+        states={
+            DEFAULT_STATE: {Main},
+        },
+    )
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
